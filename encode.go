61772233f41ee4131078a3aebe45d9646cc3205e
Viet Pham <phamhongviet52@gmail.com>
2022-05-24 22:01:02 +0700

    feat: encode

--- encode.go
+++ encode.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+)
+
 func GetCapacity(text string) int {
 	bits := 0
 	for _, c := range text {
@@ -9,5 +14,31 @@ func GetCapacity(text string) int {
 		}
 	}
 	bytes := bits/8 - 1
-	return(bytes)
+	return (bytes)
+}
+
+func bytesToBits(data []byte) string {
+	var bits bytes.Buffer
+	for _, n := range data {
+		s := fmt.Sprintf("%08b", n)
+		bits.WriteString(s)
+	}
+	return (fmt.Sprintf("%s", bits))
+}
+
+func Encode(text string, data []byte) string {
+	// TODO check capacity
+	bits := bytesToBits(data)
+	textBytes := []int32(text)
+	i := 0
+	for t, c := range textBytes {
+		v, keyExist := Codebook[c]
+		if keyExist {
+			if bits[i] == '1' {
+				textBytes[t] = v
+			}
+			i += 1
+		}
+	}
+	return (string(textBytes))
 }
