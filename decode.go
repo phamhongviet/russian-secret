3abae8e574486cb7323e2c2b5fb183c50c9f8b32
Viet Pham <phamhongviet52@gmail.com>
2022-05-24 23:16:58 +0700

    feat: check stop byte when decode

--- decode.go
+++ decode.go
@@ -1,6 +1,6 @@
 package main
 
-func Decode(text string) []byte {
+func Decode(text string, stop byte) []byte {
 	data := make([]byte, 0)
 	i := 0
 	for _, c := range text {
@@ -11,6 +11,11 @@ func Decode(text string) []byte {
 			}
 			data[i/8] = data[i/8] + (1 << (7 - i%8))
 			i += 1
+			if i%8 == 7 {
+				if data[i/8] == stop {
+					break
+				}
+			}
 		}
 		_, e2 := Codebook[c]
 		if e2 {
@@ -18,6 +23,11 @@ func Decode(text string) []byte {
 				data = append(data, 0)
 			}
 			i += 1
+			if i%8 == 7 {
+				if data[i/8] == stop {
+					break
+				}
+			}
 		}
 	}
 	return (data)
